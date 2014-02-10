0d8e165912acc9b43e469bdee478e4d070d16246
AnyaTyu <anyatyu@bk.ru>
2014-02-10 23:51:52 +0200

    7 com

--- ololo.js
+++ ololo.js
@@ -1,17 +1,20 @@
-var data = {};
-document.getElementById('button').addEventListener('click', checkLocalStorage);
-function checkLocalStorage(){
-	var user = document.getElementById('user').value;
-	user = user.toLowerCase();
-	if (localStorage.getItem(user)!= undefined) {
-		var test = JSON.parse(localStorage.getItem(user));
+document.getElementById('button').addEventListener('click', clearLocalStorage);
+
+function clearLocalStorage(){
+	for (var user in localStorage){
+		var test = (JSON.parse(localStorage.getItem(user)));
 		if (test.expiration < +new Date()) {
 			localStorage.removeItem(user);
-			findUser(user);
-		}
-		else{
+		};
+	}
+	checkLocalStorage();
+}
+
+function checkLocalStorage(){
+	var user = document.getElementById('user').value.toLowerCase();
+	if (localStorage.getItem(user)!= undefined) {
+		var test = JSON.parse(localStorage.getItem(user));
 			parseInfo(test);
-		}
 	}
 	else{
 		findUser(user);
@@ -19,8 +22,9 @@ function checkLocalStorage(){
 }
 
 function findUser(user){
+	var data = {};
 	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', 'https://api.github.com/users/'+ user, true);
+	xhttp.open('GET', 'https://api.github.com/users/'+ user);
 	xhttp.onreadystatechange = function(){
 		if (xhttp.readyState !=4){
 			return;
@@ -36,15 +40,15 @@ function findUser(user){
 		if (xhttp.status == 200) {
 			data = JSON.parse(this.response);
 			data.expiration = +new Date() + 86400000;
-			findUserRepositories(user);
+			findUserRepositories(user,data);
 		}
 	}
 	xhttp.send(null);
 }
 
-function findUserRepositories(user){
+function findUserRepositories(user,data){
 	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', 'https://api.github.com/users/' + user + "/repos", true);
+	xhttp.open('GET', 'https://api.github.com/users/' + user + "/repos");
 	
 	xhttp.onreadystatechange = function(){
 		if (xhttp.readyState !=4){
@@ -74,6 +78,7 @@ function parseInfo(userData){
 	document.getElementsByClassName('foll')[0].innerHTML = "Followers: " + userData.followers;
 	parseRepository(userData);
 }
+
 function parseRepository(userData){
 	    if (userData.repos) {
     var repos = document.getElementsByClassName('repos')[0];
