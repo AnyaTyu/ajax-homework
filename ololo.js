1cfc214758406a3be9c77021f77de7006697ce35
AnyaTyu <anyatyu@bk.ru>
2014-02-14 18:13:46 +0200

    8 com

--- ololo.js
+++ ololo.js
@@ -1,97 +1,96 @@
-document.getElementById('button').addEventListener('click', clearLocalStorage);
-
-function clearLocalStorage(){
-	for (var user in localStorage){
+var obj = {
+	clear: function(){
+		for (var user in localStorage){
 		var test = (JSON.parse(localStorage.getItem(user)));
 		if (test.expiration < +new Date()) {
-			localStorage.removeItem(user);
-		};
-	}
-	checkLocalStorage();
-}
-
-function checkLocalStorage(){
-	var user = document.getElementById('user').value.toLowerCase();
-	if (localStorage.getItem(user)!= undefined) {
+				localStorage.removeItem(user);
+			};
+		}
+		this.check();
+	},
+	check: function(){
+		var user = document.getElementById('user').value.toLowerCase();
+		if (localStorage.getItem(user)!= undefined) {
 		var test = JSON.parse(localStorage.getItem(user));
-			parseInfo(test);
-	}
-	else{
-		findUser(user);
-	}
-}
-
-function findUser(user){
-	var data = {};
-	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', 'https://api.github.com/users/'+ user);
-	xhttp.onreadystatechange = function(){
-		if (xhttp.readyState !=4){
-			return;
+			this.parse_info(test);
 		}
-
-		if  (xhttp.status == 404) {
-			document.getElementsByClassName('content')[0].style.display = "none";
-			document.getElementsByClassName('repos')[0].style.display = "none";
-			alert('Пользователь не найден');
-			return;
+		else{
+			this.find_user(user);
 		}
+	},
+	find_user: function(user){
+		var xhttp = new XMLHttpRequest();
+		self = this;
+		xhttp.open('GET', 'https://api.github.com/users/'+ user);
+		xhttp.onreadystatechange = function(){
+			if (xhttp.readyState !=4){
+				return;
+			}
 
-		if (xhttp.status == 200) {
-			data = JSON.parse(this.response);
-			data.expiration = +new Date() + 86400000;
-			findUserRepositories(user,data);
+			if  (xhttp.status == 404) {
+				document.getElementsByClassName('content')[0].style.display = "none";
+				document.getElementsByClassName('repos')[0].style.display = "none";
+				alert('Пользователь не найден');
+				return;
+			}
+
+			if (xhttp.status == 200) {
+				var data = {};
+				data = JSON.parse(this.response);
+				data.expiration = +new Date() + 86400000;
+				self.find_userRepositories(user,data);
+			}
 		}
-	}
-	xhttp.send(null);
-}
-
-function findUserRepositories(user,data){
-	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', 'https://api.github.com/users/' + user + "/repos");
+		xhttp.send();
+	},
+	find_userRepositories: function(user,data){
+		var xhttp = new XMLHttpRequest();
+		self = this;
+		xhttp.open('GET', 'https://api.github.com/users/' + user + "/repos");
 	
-	xhttp.onreadystatechange = function(){
-		if (xhttp.readyState !=4){
-			return;
-		}
+		xhttp.onreadystatechange = function(){
+			if (xhttp.readyState !=4){
+				return;
+			}
 		data.repos = JSON.parse(this.response);
 		localStorage.setItem(user,JSON.stringify(data));
-		parseInfo(data);
-	}
-	xhttp.send(null);	
-}
+		self.parse_info(data);
+		}	
+		xhttp.send();	
 
-function parseInfo(userData){
-	document.getElementsByClassName('content')[0].style.display = "block";
-	if (userData.name) {
-		document.getElementsByClassName('name')[0].innerHTML = "Name: " + userData.name;
-	}
-	else{
-		document.getElementsByClassName('name')[0].innerHTML = "";
-	}
-	if (userData.email) {
-		document.getElementsByClassName('email')[0].innerHTML = "Email: " + userData.email;
-	}
-	else{
-		document.getElementsByClassName('email')[0].innerHTML = "";
-	}
-	document.getElementsByClassName('foll')[0].innerHTML = "Followers: " + userData.followers;
-	parseRepository(userData);
-}
-
-function parseRepository(userData){
-	    if (userData.repos) {
-    var repos = document.getElementsByClassName('repos')[0];
-    			repos.style.display = "block";
-    			repos.innerHTML = "";
-                userData.repos.forEach(createLinks);
+	},
+	parse_info: function(userData){
+		document.getElementsByClassName('content')[0].style.display = "block";
+		if (userData.name) {
+			document.getElementsByClassName('name')[0].innerHTML = "Name: " + userData.name;
+		}
+		else{
+			document.getElementsByClassName('name')[0].innerHTML = "";
+		}
+		if (userData.email) {
+			document.getElementsByClassName('email')[0].innerHTML = "Email: " + userData.email;
+		}
+		else{
+			document.getElementsByClassName('email')[0].innerHTML = "";
+		}
+		document.getElementsByClassName('foll')[0].innerHTML = "Followers: " + userData.followers;
+		this.parse_repo(userData);
+	},	
+	parse_repo: function(userData){
+		if (userData.repos) {
+    	var repos = document.getElementsByClassName('repos')[0];
+    	repos.style.display = "block";
+    	repos.innerHTML = "";
+        userData.repos.forEach(createLinks);
  
-     function createLinks(element) {
-         var newLink = document.createElement('a');
-         newLink.href = element['html_url'];
-         newLink.textContent = element['name'];
-         repos.appendChild(newLink);
-         newLink.appendChild(document.createElement("br"))
-     }
-   }
+     		function createLinks(element) {
+         	var newLink = document.createElement('a');
+         	newLink.href = element['html_url'];
+         	newLink.textContent = element['name'];
+         	repos.appendChild(newLink);
+         	newLink.appendChild(document.createElement("br"))
+     		}
+   		}
+	}
 }
+document.getElementById('button').addEventListener('click', obj.clear.bind(obj));
